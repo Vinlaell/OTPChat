6795f901d738c64d8f7780d3e35962f1a97f2f4e
Vinlaell <vinlaell@gmail.com>
2012-05-07 03:09:43 -0400

    Initial commit.
    functional TCP chat with OTP encryption, pad generation, automatic
    deletion of used pad contents. but very unpolished and needs:
    rewriting of TCP side.
    not deleting used pad pieces if message fails to be sent(if secure).
    notification to sender if message was recieved.

---
+++ OTPChat/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace OTPChat
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 58);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(484, 95);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Click += new System.EventHandler(this.textBox1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(166, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Listen port:";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 234);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(485, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(86, 22);
+            this.toolStripLabel1.Text = "toolStripLabel1";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(0, 153);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(484, 47);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(289, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Listen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(68, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "IP:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(94, 26);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(89, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.Text = "127.0.0.1";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(13, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(616, 23);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(231, 0);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(52, 20);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "40000";
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(0, 206);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(484, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Send";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(409, 3);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "PadGen";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click_1);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(231, 26);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(52, 20);
+            this.textBox5.TabIndex = 16;
+            this.textBox5.Text = "40000";
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 29);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Send to:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(189, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "Port:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 259);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "Form1";
+            this.Text = "OTPChat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ OTPChat/Form1.cs
@@ -0,0 +1,350 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.IO;
+using System.Security.Cryptography;
+
+namespace OTPChat
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private TcpListener tcpListener;
+        private Thread listenThread;
+
+        string modnum(int c)
+        {
+            string result = "";
+            switch (c)
+            {
+                case 1: result = "a"; break;
+                case 2: result = "b"; break;
+                case 3: result = "c"; break;
+                case 4: result = "d"; break;
+                case 5: result = "e"; break;
+                case 6: result = "f"; break;
+                case 7: result = "g"; break;
+                case 8: result = "h"; break;
+                case 9: result = "i"; break;
+                case 10: result = "j"; break;
+                case 11: result = "k"; break;
+                case 12: result = "l"; break;
+                case 13: result = "m"; break;
+                case 14: result = "n"; break;
+                case 15: result = "o"; break;
+                case 16: result = "p"; break;
+                case 17: result = "q"; break;
+                case 18: result = "r"; break;
+                case 19: result = "s"; break;
+                case 20: result = "t"; break;
+                case 21: result = "u"; break;
+                case 22: result = "v"; break;
+                case 23: result = "w"; break;
+                case 24: result = "x"; break;
+                case 25: result = "y"; break;
+                case 26: result = "z"; break;
+            }
+            return result;
+        }
+
+        int modchar(char c)
+        {
+            int result = 0;
+            switch (c)
+            {
+                case 'a': result = 1; break;
+                case 'b': result = 2; break;
+                case 'c': result = 3; break;
+                case 'd': result = 4; break;
+                case 'e': result = 5; break;
+                case 'f': result = 6; break;
+                case 'g': result = 7; break;
+                case 'h': result = 8; break;
+                case 'i': result = 9; break;
+                case 'j': result = 10; break;
+                case 'k': result = 11; break;
+                case 'l': result = 12; break;
+                case 'm': result = 13; break;
+                case 'n': result = 14; break;
+                case 'o': result = 15; break;
+                case 'p': result = 16; break;
+                case 'q': result = 17; break;
+                case 'r': result = 18; break;
+                case 's': result = 19; break;
+                case 't': result = 20; break;
+                case 'u': result = 21; break;
+                case 'v': result = 22; break;
+                case 'w': result = 23; break;
+                case 'x': result = 24; break;
+                case 'y': result = 25; break;
+                case 'z': result = 26; break;
+            }
+            return result;
+        }
+        void padgen()
+        {
+            using (RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider())
+            {
+                uint value;
+                byte[] data = new byte[4096];
+                System.IO.StreamWriter file = new System.IO.StreamWriter("pad.txt");
+                System.IO.StreamWriter file2 = new System.IO.StreamWriter("pad2.txt");
+
+
+                for (int i = 0; i < 1000; i++)
+                {
+                    rng.GetBytes(data);
+                    value = BitConverter.ToUInt32(data, 0);
+                    file.Write(value);
+                    file2.Write(value);
+                }
+                file.Close();
+                file2.Close();
+
+            }
+        }
+
+        string cipher(string msg)
+        {
+            string textread = System.IO.File.ReadAllText(@"pad.txt");
+            string padmatch = textread.Substring(0, msg.Length);
+            string ctext = "";
+            int[] resa = new int[26];
+            int[] resb = new int[26];
+            if (System.Text.RegularExpressions.Regex.IsMatch(msg, @"^[a-z]+$")) { }
+            else
+            {
+                MessageBox.Show("input is not regular lowercase letters with no spaces!");
+                return "";
+            }
+            for (int i = 0; i < msg.Length; i++)
+            {
+                resa[i] = modchar(msg[i]);
+                resb[i] = resa[i] + Convert.ToInt32(padmatch.Substring(i, 1));
+                if (resb[i] > 26) resb[i] = resb[i] - 26;
+            }
+            foreach (int i in resb)
+            {
+                ctext = ctext + modnum(i).ToString();
+            }
+            //MessageBox.Show("ciphering "+msg);
+            textread = textread.Substring(msg.Length, textread.Length - msg.Length);
+            System.IO.StreamWriter file = new System.IO.StreamWriter("pad.txt");
+            file.Write(textread);
+            file.Close();
+            return ctext;
+        }
+
+        string decipher(string msg)
+        {
+            string textread = System.IO.File.ReadAllText(@"pad2.txt");
+            string padmatch = textread.Substring(0, msg.Length);
+            string ctext = "";
+            int[] resa = new int[26];
+            int[] resb = new int[26];
+            for (int i = 0; i < msg.Length; i++)
+            {
+                resa[i] = modchar(msg[i]);
+                resb[i] = resa[i] - Convert.ToInt32(padmatch.Substring(i, 1));
+                if (resb[i] < 1) resb[i] = resb[i] + 26;
+            }
+            foreach (int i in resb)
+            {
+                ctext = ctext + modnum(i).ToString();
+            }
+            //MessageBox.Show("deciphering " + msg);
+
+            textread = textread.Remove(0, msg.Length);
+            System.IO.StreamWriter file = new System.IO.StreamWriter("pad2.txt");
+            file.Write(textread);
+            file.Close();
+            return ctext;
+        }
+
+        public void Sendmsg()
+        {
+            TcpClient client = new TcpClient();
+
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(Properties.Settings.Default.ip), Convert.ToInt32(Properties.Settings.Default.port2));
+
+            client.Connect(serverEndPoint);
+
+            NetworkStream clientStream = client.GetStream();
+
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            byte[] buffer = encoder.GetBytes(cipher(textBox2.Text));
+            textBox1.Text = textBox1.Text + "[me] " + textBox2.Text + Environment.NewLine;
+            clientStream.Write(buffer, 0, buffer.Length);
+            clientStream.Flush();
+            
+        }
+
+        public void Server()
+        {
+            this.tcpListener = new TcpListener(IPAddress.Any, Convert.ToInt32(Properties.Settings.Default.port));
+            this.listenThread = new Thread(new ThreadStart(ListenForClients));
+            this.listenThread.Start();
+        }
+
+        private void ListenForClients()
+        {
+            this.tcpListener.Start();
+
+            while (true)
+            {
+                //blocks until a client has connected to the server
+                TcpClient client = this.tcpListener.AcceptTcpClient();
+                //create a thread to handle communication 
+                //with connected client
+                Thread clientThread = new Thread(new ParameterizedThreadStart(HandleClientComm));
+                clientThread.Start(client);
+            }
+        }
+
+        private void HandleClientComm(object client)
+        {
+            TcpClient tcpClient = (TcpClient)client;
+            NetworkStream clientStream = tcpClient.GetStream();
+
+            byte[] message = new byte[4096];
+            int bytesRead;
+
+            while (true)
+            {
+                bytesRead = 0;
+
+                try
+                {
+                    //blocks until a client sends a message
+                    bytesRead = clientStream.Read(message, 0, 4096);
+                }
+                catch
+                {
+                    //a socket error has occured
+                    break;
+                }
+
+                if (bytesRead == 0)
+                {
+                    //the client has disconnected from the server
+                    break;
+                }
+
+                //message has successfully been received
+                ASCIIEncoding encoder = new ASCIIEncoding();
+                string msg = encoder.GetString(message, 0, bytesRead);
+                //System.Diagnostics.Debug.WriteLine(encoder.GetString(message, 0, bytesRead));
+                
+                    Properties.Settings.Default.message = msg;
+
+                
+            }
+
+            //tcpClient.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            toolStripLabel1.Text = "Idle";
+            textBox3.Text = Properties.Settings.Default.ip;
+            textBox4.Text = Properties.Settings.Default.port;
+            textBox5.Text = Properties.Settings.Default.port2;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            //listen
+            toolStripLabel1.Text = "Listening..";
+            Server();
+
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+            Properties.Settings.Default.message = "";
+            Properties.Settings.Default.msg2 = "";
+            Properties.Settings.Default.Save();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (Properties.Settings.Default.message != Properties.Settings.Default.msg2)
+            {
+                Properties.Settings.Default.msg2 = Properties.Settings.Default.message;
+                //MessageBox.Show(Properties.Settings.Default.message);
+
+                textBox1.AppendText(decipher(Properties.Settings.Default.message) + Environment.NewLine);
+                
+
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+            Sendmsg();
+        }
+
+        private void textBox1_Click(object sender, EventArgs e)
+        {
+            //if (Properties.Settings.Default.message != Properties.Settings.Default.msg2)
+            //{
+            //textBox1.AppendText(Properties.Settings.Default.message);
+            //}
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //toolStripLabel1.Text = "Connecting..";
+            //button1.Text = "Cancel";
+            //connect
+            //
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.port = textBox4.Text;
+        }
+
+        private void button5_Click_1(object sender, EventArgs e)
+        {
+            DialogResult result;
+            result = MessageBox.Show("Are you sure you want to generate a new pad?", "Pad Generator", MessageBoxButtons.YesNo);
+            if (result == DialogResult.No)
+            {
+
+            }
+            if (result == DialogResult.Yes)
+            {
+                padgen();
+            }
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.port2 = textBox5.Text;
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Properties.Settings.Default.ip = textBox3.Text;
+        }
+    }
+}
